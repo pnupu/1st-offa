8fd86675ebefa5529c76de754c857d3494d6d596
miikkalaitinen <38249083+miikkalaitinen@users.noreply.github.com>
2024-11-08 19:33:56 +0200

    start game wrapper

---
+++ src/app/game/_components/GameWrapper.tsx
@@ -0,0 +1,9 @@
+const GameWrapper = () => {
+    return (
+        <div className="bg-white w-[1280px] h-[720px]">
+          
+        </div>
+    )
+}
+
+export default GameWrapper;
---
+++ src/app/game/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import GameWrapper from "./_components/GameWrapper";
+
+const GamePage = () => {
+  return (
+    <div style={{ textAlign: 'center', height: "100vh", width: "100vw", background: "grey" }}>
+      <GameWrapper />
+    </div>
+  );
+};
+
+export default GamePage;
