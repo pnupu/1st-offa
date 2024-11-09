77b2b4aa6a051d0b450e1e3a13cb4baccfb4ca1b
miikkalaitinen <38249083+miikkalaitinen@users.noreply.github.com>
2024-11-09 16:20:33 +0200

    BowerBoint on valmis

--- src/app/game/_components/computer/apps/Bower.tsx
+++ src/app/game/_components/computer/apps/Bower.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Bower = () => {
 
-  const [ openSlideIndex, setOpenSlideIndex ] = useState<number>(0);
+  const [ openSlideIndex, setOpenSlideIndex ] = useState<number>(1);
+
 
   return (
     <div className="h-full flex flex-col">
@@ -29,27 +30,37 @@ const Bower = () => {
       </div>
       <div className="w-full h-[230px] bg-gray-100 flex">
         <div className="w-[140px] bg-gray-200 border-r border-gray-200 overflow-y-auto"> 
-          <div className="flex justify-between items-center px-4 py-2 cursor-pointer hover:bg-gray-100" onClick={() => setOpenSlideIndex(0)}>
-            <Image src="/assets/computer_backgrounds/kisse.png" width={100} height={50} alt="Slide" />
-          </div>
           <div className="flex justify-between items-center px-4 py-2 cursor-pointer hover:bg-gray-100" onClick={() => setOpenSlideIndex(1)}>
-            <Image src="/assets/computer_backgrounds/kisse.png" width={100} height={50} alt="Slide" />
+            <p className="text-gray-900 text-xs ml-[-5px] mr-[5px]">1.</p>
+            <Image src={`/assets/img/Presentation/ProjectX_1.png`} width={100} height={50} alt="Slide" />
           </div>
           <div className="flex justify-between items-center px-4 py-2 cursor-pointer hover:bg-gray-100" onClick={() => setOpenSlideIndex(2)}>
-            <Image src="/assets/computer_backgrounds/kisse.png" width={100} height={50} alt="Slide" />
+            <p className="text-gray-900 text-xs ml-[-5px] mr-[5px]">2.</p>
+            <Image src={`/assets/img/Presentation/ProjectX_2.png`} width={100} height={50} alt="Slide" />
           </div>
           <div className="flex justify-between items-center px-4 py-2 cursor-pointer hover:bg-gray-100" onClick={() => setOpenSlideIndex(3)}>
-            <Image src="/assets/computer_backgrounds/kisse.png" width={100} height={50} alt="Slide" />
+           <p className="text-gray-900 text-xs ml-[-5px] mr-[5px]">3.</p>
+            <Image src={`/assets/img/Presentation/ProjectX_3.png`} width={100} height={50} alt="Slide" />
           </div>
           <div className="flex justify-between items-center px-4 py-2 cursor-pointer hover:bg-gray-100" onClick={() => setOpenSlideIndex(4)}>
-            <Image src="/assets/computer_backgrounds/kisse.png" width={100} height={50} alt="Slide" />
+            <p className="text-gray-900 text-xs ml-[-5px] mr-[5px]">4.</p>
+            <Image src={`/assets/img/Presentation/ProjectX_4.png`} width={100} height={50} alt="Slide" />
           </div>
           <div className="flex justify-between items-center px-4 py-2 cursor-pointer hover:bg-gray-100" onClick={() => setOpenSlideIndex(5)}>
-            <Image src="/assets/computer_backgrounds/kisse.png" width={100} height={50} alt="Slide" />
+            <p className="text-gray-900 text-xs ml-[-5px] mr-[5px]">5.</p>
+            <Image src={`/assets/img/Presentation/ProjectX_5.png`} width={100} height={50} alt="Slide" />
           </div>
           <div className="flex justify-between items-center px-4 py-2 cursor-pointer hover:bg-gray-100" onClick={() => setOpenSlideIndex(6)}>
-            <Image src="/assets/computer_backgrounds/kisse.png" width={100} height={50} alt="Slide" />
+            <p className="text-gray-900 text-xs ml-[-5px] mr-[5px]">6.</p>
+            <Image src={`/assets/img/Presentation/ProjectX_6.png`} width={100} height={50} alt="Slide" />
           </div>
+          <div className="flex justify-between items-center px-4 py-2 cursor-pointer hover:bg-gray-100" onClick={() => setOpenSlideIndex(7)}>
+            <p className="text-gray-900 text-xs ml-[-5px] mr-[5px]">7.</p>
+            <Image src={`/assets/img/Presentation/ProjectX_7.png`} width={100} height={50} alt="Slide" />
+          </div>
+        </div>
+        <div className="w-[515px] h-full bg-white flex justify-center items-center">
+          <Image src={`/assets/img/Presentation/ProjectX_${openSlideIndex}.png`} width={400} height={200} alt="Slide" />
         </div>
       </div>
     </div>
