217d7d4ac7bf38df9d45c9a22a31c6fc606632ba
miikkalaitinen <38249083+miikkalaitinen@users.noreply.github.com>
2024-11-09 12:52:27 +0200

    fix: working clock and fixed computer

--- src/app/game/_components/static_components/Clock.tsx
+++ src/app/game/_components/static_components/Clock.tsx
@@ -1,10 +1,25 @@
 import Image from "next/image";
+import { useGameTime } from "../GameTimeContext";
 
 const Clock = () => {
+  const { timeHours, timeMinutes } = useGameTime();
+
+  const hourDegrees = (timeHours % 12) * 30 + (timeMinutes / 60) * 30; // 360 degrees / 12 hours
+  const minuteDegrees = timeMinutes * 6; // 360 degrees / 60 minutes
 
   return (
     <div className="w-full h-full flex items-center justify-center" onClick={() => console.log("CLOCK")}>
       <Image src="/assets/desktop/Clock.svg" alt="Clock" className="w-full h-full object-contain" draggable="false" layout="fill" />
+      <div
+        id="hourHand"
+        className="absolute !w-[3px] !h-[35px] !top-[40px] !left-[74px] bg-black"
+        style={{ transform: `rotate(${hourDegrees}deg)`, transformOrigin: 'bottom center' }}
+      />
+      <div
+        id="minuteHand"
+        className="absolute !w-[3px] !h-[58px] !top-[17px] !left-[74px] bg-black"
+        style={{ transform: `rotate(${minuteDegrees}deg)`, transformOrigin: 'bottom center' }}
+      />
     </div>
   );
 }
